56cf8db5de3a684be0bae88ad218ecd586310a42
Sam Forbyn <samforbyn@gmail.com>
2021-11-20 07:42:04 -0700

    changed arr placeholder

--- weekTwoChallenge.js
+++ weekTwoChallenge.js
@@ -5,16 +5,33 @@
 
 // Sample input const myArray = [1, 2, 3, 5, 6, 8, 9] ---- Should return [1, 4, 9, 25, 36, 64, 81]
 
-const squareSquasher = arr => {
-    newArr = []
-    for(let x of arr){
-        let newVal = x*x
-        newArr.push(newVal)
-    }
-    return newArr
+
+let testArr = [1, 2, 3, 5, 6, 8, 9]
+
+// VERISON 1 //
+
+// const squareSquasher = arr => {
+//     newArr = []
+//     for(let x of arr){
+//         let newVal = x*x
+//         newArr.push(newVal)
+//     }
+//     return newArr
+// }
+
+
+// VERISON 2
+
+const sqrSqsh = arr =>{
+    return arr.map(function (x){
+        return Math.pow(x, 2)
+    })
 }
 
-console.log(squareSquasher([1, 2, 3, 5, 6, 8, 9]))
+
+// console.log(squareSquasher(testArr))  
+
+console.log(sqrSqsh(testArr))             
 
 
 
